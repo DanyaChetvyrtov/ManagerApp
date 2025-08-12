daa203ec98c62c60c18ffb6c0e394c3e5f72b0f1
Danil <dabcom2013@gmail.com>
2025-08-12 13:30:15 +0300

    service. Add manager-ms

---
+++ services/manager-ms/src/main/java/ru/ex/managerms/ManagerMsApplication.java
@@ -0,0 +1,13 @@
+package ru.ex.managerms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerMsApplication.class, args);
+	}
+
+}
---
+++ services/manager-ms/src/test/java/ru/ex/managerms/ManagerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ex.managerms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
