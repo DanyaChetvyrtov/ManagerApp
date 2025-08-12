d3fe23bb4a2126b43a3118523cbc0ef4f4848116
Danil <dabcom2013@gmail.com>
2025-08-12 18:10:55 +0300

    catalogue-ms. Create ms. Finish with CRUD

---
+++ services/catalogue-ms/src/main/java/ru/ex/cataloguems/CatalogueMsApplication.java
@@ -0,0 +1,13 @@
+package ru.ex.cataloguems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogueMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogueMsApplication.class, args);
+	}
+
+}
---
+++ services/catalogue-ms/src/test/java/ru/ex/cataloguems/CatalogueMsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ex.cataloguems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogueMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
