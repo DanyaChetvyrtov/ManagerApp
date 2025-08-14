abf9d44297f023d4932550d63b50f59c04d3b848
Danil <dabcom2013@gmail.com>
2025-08-14 16:09:23 +0300

    catalogue-ms. Add basic security.

---
+++ services/catalogue-ms/src/main/java/ru/ex/cataloguems/configurations/SecurityConfig.java
@@ -0,0 +1,23 @@
+package ru.ex.cataloguems.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/catalogue-api/**").hasRole("SERVICE"))
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
